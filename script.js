cfd513313fb8afe8be69888bfbd3217227754fd6
hanbonghun <bng4535@khu.ac.kr>
2022-03-15 04:50:56 +0900

    구현 기능들 fabirc으로 수정

--- script.js
+++ script.js
@@ -1,44 +1,81 @@
-//html 파일 로드 되었을 때(초기화면)
-window.addEventListener("load",()=>{
-    var canvas = document.getElementById('canvas');
-    canvas.height = 400;
-    canvas.width = 800;
-    if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+var canvas;
 
-    ctx.fillStyle = 'rgb(255, 255, 255)';
-    ctx.fillRect(0,0,1500, 600);
-    }
+//마우스 위치
+var currentMouseX;
+var currentMouseY;
+
+//html 파일 로드 되었을 때(초기화면)
+window.addEventListener("load", () => { //canvas : fabric 객체를 관리하는 객체
+    canvas = new fabric.Canvas('c', {
+        backgroundColor: "white"
+    });
+    canvas.setHeight(400);
+    canvas.setWidth(800);
+
+    canvas.on(
+        'object:added', function () {
+            console.log("추가");
+            canvas.setActiveObject(canvas.item(canvas.getObjects().length - 1)); //객체 생성 시 setActive
+        });
+    canvas.on(
+        'object:modified', function () {
+            console.log("수정");
+        });
+
+    canvas.on('mouse:wheel', function (opt) {
+        var delta = opt.e.deltaY;
+        var zoom = canvas.getZoom();
+        zoom *= 0.999 ** delta;
+        if (zoom > 20) zoom = 20;
+        if (zoom < 0.01) zoom = 0.01;
+        canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+        opt.e.preventDefault();
+        opt.e.stopPropagation();
+    });
+
 })
 
+
+
+function clearCanvas() { //캔버스 초기화 
+    canvas.clear();
+    canvas.setBackgroundColor("white");
+    // canvas = new fabric.Canvas('c', {
+    //     backgroundColor: "white"
+    // });
+    // canvas.setHeight(400);
+    // canvas.setWidth(800);
+}
+
 //이미지 첨부
-function selectImage(){
-    var canvas =document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var reader = new FileReader();
-    var img = new Image(); //이미지 처리 객체 생성 
-    const uploadImage = (e) =>{
-        reader.onload = () =>{
-            img.onload = () =>{
-                canvas.width= img.width;
-                canvas.height=img.height;
-                // ctx.drawImage(img,(canvas.width-img.width)/2,(canvas.height-img.height)/2); //캔버스에 이미지 write 
-                ctx.drawImage(img,0,0); //캔버스에 이미지 write 
-            };
-            img.src = reader.result;
-        };
-        reader.readAsDataURL(e.target.files[0]);
-    };
-    const imageLoader = document.getElementById("choose-file");
-    imageLoader.addEventListener("change",uploadImage);
+function selectImage() {
+
+    document.getElementById("choose-file").onchange = function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) { //onload(): 읽기 성공 시 실행되는 핸들러
+            var image = new Image();
+            image.src = e.target.result;
+            image.onload = function () {
+                var img = new fabric.Image(image);
+                img.set({
+
+                    left: 100,
+                    top: 60
+                });
+                img.scaleToWidth(200);
+                canvas.add(img).setActiveObject(img).renderAll();
+            }
+        }
+        reader.readAsDataURL(e.target.files[0]); // dataURL 형식으로 파일 읽음
+    }
 }
 
 //이미지 다운로드
-function downloadImage(){
+function downloadImage() {
     var image = canvas.toDataURL();  //canvas 그림을 문자열 형태로 
     var link = document.createElement('a'); //<a> 생성
-    link.href= image;
-    link.download= "image.png";
+    link.href = image;
+    link.download = "image.png";
     link.click();
 }
 
@@ -46,41 +83,72 @@ function downloadImage(){
 
 
 //그리기(펜)
-var painting =false; 
 
-function changeCursor(){
-    document.getElementById("canvas").style.cursor = "crosshair";
-}
-
-function startPosition(e){
-    
-    painting = true; 
-    draw(e);
-}
-
-function finishedPosition(){
-    painting=  false;
-    ctx.beginPath();    
-}
-
-function draw(e){
-    if(!painting) return ;
-    ctx.lineWidth = 5 ; 
-    ctx.lineCap = 'round';
-    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY-canvas.offsetTop);
-    ctx.stroke(); //그리기 
-    ctx.beginPath(); //경로 시작 
-    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY-canvas.offsetTop);
-}
-
-function drawLine(){
-    changeCursor();
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    
-    canvas.addEventListener("mousedown",startPosition);
-    canvas.addEventListener("mouseup",finishedPosition);
-    canvas.addEventListener("mousemove",draw);
+function drawLine() {
+    console.log(canvas);
+    canvas.isDrawingMode = true;
+    if (canvas.isDrawingMode == true) {
+        console.log("켜짐");
+    }
+    canvas.on('mouse:up', function () {
+        canvas.isDrawingMode = false;
+    });
 
 }
 
+
+//도형삽입 
+function drawRectangle() {
+    canvas.add(new fabric.Rect({
+        left: Math.floor(Math.random() * 101),
+        top: Math.floor(Math.random() * 101),
+        fill: '#f55',
+        width: 50,
+        height: 50
+    }));
+
+}
+
+function drawCircle() {
+    canvas.add(new fabric.Circle({
+        radius: 30, fill: '#f55',
+        top: Math.floor(Math.random() * 101),
+        left: Math.floor(Math.random() * 101)
+    }));
+}
+
+
+function test() {
+    console.log("ㅇㅋ");
+    console.log(disableMovement);
+    console.log(selection);
+    canvas.on('mouse:down', function (o) {
+        if (disableMovement || selection) return;
+        isDown = true;
+        console.log('down')
+        var pointer = canvas.getPointer(o.e);
+        origX = pointer.x;
+        origY = pointer.y;
+        var pointer = canvas.getPointer(o.e);
+        rect = new fabric.Rect({
+            left: origX,
+            top: origY,
+            originX: 'left',
+            originY: 'top',
+            width: pointer.x - origX,
+            height: pointer.y - origY,
+            angle: 0,
+            fill: 'rgba(255,0,0,0.5)',
+            transparentCorners: false,
+        });
+        canvas.add(rect);
+    });
+}
+//text박스 삽입
+function textBox() {
+    var text = new fabric.Textbox('A Computer Science Portal',
+        {
+            width: 450
+        });
+    canvas.add(text);
+}
