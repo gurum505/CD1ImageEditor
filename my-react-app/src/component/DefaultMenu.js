e98290b7eb501c678f7ee759bed45653b6f7c451
hanbonghun <bng4535@khu.ac.kr>
2022-03-25 16:57:56 +0900

    DefaultMenu : canvas 크기 조절 컴포넌트 추가

---
+++ my-react-app/src/component/DefaultMenu.js
@@ -0,0 +1,22 @@
+export default function DefaultMenu(props) {
+    
+    const canvas = props.canvas;
+    
+    function resizeCanvasWidth(e){
+        canvas.setWidth(e.target.value);
+        console.log(e.target.value);
+    }
+    function resizeCanvasHeight(e){
+        canvas.setHeight(e.target.value);
+        console.log(e.target.value);
+    }
+    return (<>
+        <div>
+            <label>canvas width: </label>
+            <input type="text"  onChange = {resizeCanvasWidth} placeholder="800" / >
+            <br />
+            <label>canvas height: </label>
+            <input type="text"  onChange={resizeCanvasHeight} placeholder="600" />
+        </div>
+    </>)
+}
